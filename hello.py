4847ad02cd953ab37134b4c7502053720deacb81
ZakiDhiblawe <zakidhiblawe1243@gmail.com>
2024-09-02 20:16:04 +0300

    add database

--- hello.py
+++ hello.py
@@ -1,15 +1,41 @@
 from flask import Flask, redirect, render_template, flash
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import EmailField, IntegerField, StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # create instance fro flask
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SECRET_KEY']= 'you secret key'
 
 class Zaki(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+
+
+# create database
+db = SQLAlchemy(app)
+
+# create Module
+class Users(db.Model):
+    id=db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name= db.Column(db.String(200), nullable=False)
+    age= db.Column(db.Integer, nullable=False)
+    email= db.Column(db.String(200), nullable=False, unique=True)
+    date_added= db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
+    
+class UsersForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    age = IntegerField('Age', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 
 @app.route('/')
 def index():
@@ -27,6 +53,37 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+
+@app.route('/user/add', methods=['GET', 'POST'])
+def add_user():
+    name = None
+    form = UsersForm()
+    our_users = Users.query.order_by(Users.date_added).all()  # Ensure `our_users` is always defined
+
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = Users(name=form.name.data, age=form.age.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+            flash('User added successfully')
+        else:
+            flash('User email already exists' , 'danger')
+        name = form.name.data
+        form.name.data = ''
+        form.age.data = ''
+        form.email.data = ''
+        
+        # Update `our_users` after adding a new user
+        our_users = Users.query.order_by(Users.date_added).all()
+
+         # Format dates before passing to the template
+        for user in our_users:
+            user.formatted_date = user.date_added.strftime('%Y-%m-%d %H:%M:%S')
+
+    return render_template('add.html', form=form, name=name, our_users=our_users)
+
 
 
 
@@ -56,3 +113,9 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template('500.html'), 500
+
+
+# if __name__ == "__main__":
+#     with app.app_context():
+#         db.create_all()
+#         print("Database tables created successfully!")
